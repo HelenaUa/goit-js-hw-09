44a4d64ecb74d70fbcfc7effbf22aa8113132978
HelenaUa <delena.1408@gmail.com>
2022-12-30 21:06:18 +0200

    add task01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -9,4 +9,25 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
 
+const body = document.querySelector("body");
+const btnStart = document.querySelector("button[data-start]");
+const btnStop = document.querySelector("button[data-stop]");
+let timerId = null;
 
+btnStart.addEventListener("click", onClickStart);
+btnStop.addEventListener("click", onClickStop);
+
+function onClickStart(event) {
+    timerId = setInterval(() => {
+        const randomColor = getRandomHexColor();
+        body.style.backgroundColor = randomColor;
+        btnStart.disabled = true;
+        btnStop.disabled = false;
+    },1000);
+};
+
+function onClickStop(event) {
+    clearInterval(timerId);
+    btnStop.disabled = true;
+    btnStart.disabled = false;
+};
